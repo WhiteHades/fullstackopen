79f8187c3fa88c278b5286d2170c06f98b34cc85
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2024-08-05 00:16:41 +0200

    added comps

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = ({ header }) => {
+  return <h1>{header}</h1>;
+};
+
+const Header2 = ({ header2 }) => {
+  return <h1>{header2}</h1>;
+};
+
+const Button = ({ button1, button2, button3 }) => {
+  return (
+    <div>
+      <button>{button1}</button>
+      <button>{button2}</button>
+      <button>{button3}</button>
+    </div>
+  );
+};
+
+const Statistics = ({ good, bad, neutral }) => {
+  return (
+    <div>
+      <p> good {good}</p>
+      <p> bad {bad}</p>
+      <p> neutral {neutral}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const header = "give feedback";
+  const header2 = "statistics";
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header header={header} />
+      <Button button1={good} button2={neutral} button3={bad} />
+      <Header2 header2={header2} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
 
-export default App
+export default App;
