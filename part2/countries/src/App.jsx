6b801526a7bc81f74b3c568c317d607090df372c
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2024-08-11 01:37:43 +0200

    ex 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Message from "./component/Message";
+import Country from "./component/Country";
+import CountryOne from "./component/CountryOne";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [message, setMessage] = useState("");
+  let content;
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then((response) => {
+          const filteredCountries = response.data.filter((country) =>
+            country.name.common.toLowerCase().includes(name.toLowerCase())
+          );
+          setCountries(filteredCountries);
+          filteredCountries.length > 10
+            ? setMessage("Too many matches, specify another filter")
+            : setMessage("");
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [name]);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        find countires{" "}
+        <input
+          value={name}
+          onChange={handleNameChange}
+        ></input>
+        {message ? (
+          <Message message={message} />
+        ) : countries.length === 1 ? (
+          <CountryOne country={countries[0]} />
+        ) : (
+          <Country countries={countries} />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ part2/countries/src/component/CountryOne.jsx
@@ -0,0 +1,22 @@
+const CountryOne = ({ country }) => {
+  console.log(Object.values(country.languages));
+  return (
+    <>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+      <h4>languages</h4>
+      <ul>
+        {Object.values(country.languages).map((language, index) => {
+          return <li key={index}>{language}</li>;
+        })}
+      </ul>
+      <img
+        src={country.flags.png}
+        width="150"
+      />
+    </>
+  );
+};
+
+export default CountryOne;
